78ff850371e499bf5288c15d84656f344fb939a8
VanesaGC <vanesa.garcia.c@gmail.com>
2019-03-31 23:58:54 +0000

    Aplicación funcionando en AWS

--- deployment/ssh/deploy.sh
+++ deployment/ssh/deploy.sh
@@ -5,4 +5,4 @@ USER="ec2-user"
 KEY="deployment.pem"
 
 ssh -i $KEY $USER@$HOST \
-    "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8888 cloud-lab"
+    "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8888 vanesagcdocker/cloud-lab"
