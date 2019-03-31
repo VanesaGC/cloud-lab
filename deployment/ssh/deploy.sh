a9c72c793e20b75a0d1ef2e2c6527417e3f89bb1
VanesaGC <vanesa.garcia.c@gmail.com>
2019-03-31 23:35:22 +0000

    Creación servidor y problema al desplegar imágen de docker en AWS

---
+++ deployment/ssh/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOST="13.58.17.245"
+USER="ec2-user"
+KEY="deployment.pem"
+
+ssh -i $KEY $USER@$HOST \
+    "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8888 cloud-lab"
